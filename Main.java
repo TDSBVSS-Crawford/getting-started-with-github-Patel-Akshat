b972b2062978dacf10289be19457cea48bbb92e7
Patel-Akshat <113193080+Patel-Akshat@users.noreply.github.com>
2022-09-16 08:54:28 -0700

    Java file about lesson 1

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
-import javax.print.event.PrintEvent;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.print("Goodbye!");
+        System.out.println("Hello");
+        /*Comment */
     }
 }
